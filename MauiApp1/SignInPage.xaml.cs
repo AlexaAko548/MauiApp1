c01a2f7ddc6c72bdc576abb629cebcb45f2efdd7
AlexaAko548 <160448789+AlexaAko548@users.noreply.github.com>
2025-04-03 21:21:57 +0800

    signin and signup pages

---
+++ MauiApp1/SignInPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1
+{
+    public partial class SignInPage : ContentPage
+    {
+        public SignInPage()
+        {
+            InitializeComponent();
+        }
+
+        // Navigate to the SignUpPage
+        private async void SignUpButton_Clicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//SignUpPage");
+        }
+    }
+}
---
+++ MauiApp1/SignUpPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1
+{
+    public partial class SignUpPage : ContentPage
+    {
+        public SignUpPage()
+        {
+            InitializeComponent();
+        }
+
+        // Navigate back to the SignInPage
+        private async void GoBackButton_Clicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//SignInPage");
+        }
+    }
+}
